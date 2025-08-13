0d4362fa5ab082c1a588c4d1ae3ea104221a3d4b
vghdse <h.hanayan66@gmail.com>
2025-08-13 18:57:48 +0200

    Update settings.js for PHOENIX

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;6UdBUDrR#vaRys5_1c-DrinMqIw-ScSY5RRxvgMHP_Upbf-M-Nhk",
+  OWNER_NUMBER: "+27733981843", 
+  PREFIX: "!"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
