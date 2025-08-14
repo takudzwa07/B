e988acd96bee024d6a15bedd8823682d77916226
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-15 00:02:24 +0200

    Update settings.js for Tiflow

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;fMcWjJII#RVCb4j5Tt-faznvo0TS4rZuq-VSMMXIpHZrkuUTC22M",
+  OWNER_NUMBER: "+237 6 93 73 38 69", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
