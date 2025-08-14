b9edd3292677efe5dafd130fd16d0bc98bac4e8b
vghdse <h.hanayan66@gmail.com>
2025-08-14 11:29:54 +0200

    Update settings.js for VENOM

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;qUEXiQCR#HoZc9g9Os8QU80Xk5GJjxg3LXdxffiesMszVkFmZFNc",
+  OWNER_NUMBER: "+27733981843", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
