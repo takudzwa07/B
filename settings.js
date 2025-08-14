502036d8c34d6259ee25e8b4afc99be38dce27b3
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-14 23:45:49 +0200

    Update settings.js for Shunsui-Kyoraku

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;SYlQnS7S#7VzwhFRpYQM9WIM4HPT-TgAAcuuMiS3hoBzFC_iq9tI",
+  OWNER_NUMBER: "+237658823604", 
+  PREFIX: ">"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
