44960f81f37001b849440a077e9f9281337075f2
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-15 10:36:48 +0200

    Update settings.js for olenga

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;SZcVlQRY#TL_Ygm4PqeRYYCoh0rPSC3iatLjXQIBtrkYr7YP3pvM",
+  OWNER_NUMBER: "+255764499686 ", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
