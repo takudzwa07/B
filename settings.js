4ded03b8196ffdaf8ddbdf1a4f0ad990e62753a3
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-17 09:43:15 +0200

    Update settings.js for Zero

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;nZdVjCxA#lY8Hefz0R-5oS4X3PZAV3i9R3GA_eAafbSLc-Wayn88",
+  OWNER_NUMBER: "+27660899221", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
