174748b5bb21ee2adc82c386ed75d9b64affb912
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-17 09:18:58 +0200

    Update settings.js for Maskio

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;rMkhTRKb#69252SUmnm4Chx6ybuFtHlnho0joc1F7oxgtT4pQXXA",
+  OWNER_NUMBER: "+254750735511", 
+  PREFIX: "§"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
